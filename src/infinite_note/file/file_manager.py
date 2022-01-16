54a3520765b1a61d4f8a52259d499cd10d03c0a7
offtheface <rokany@cau.ac.kr>
2022-01-16 17:51:54 +0900

    파일 추가 테스트 적용

--- src/infinite_note/file/file_manager.py
+++ src/infinite_note/file/file_manager.py
@@ -1,16 +1,23 @@
+import os.path
 
-def save_question(msg):
+def save_question(msg, file_name='inf-question.txt'):
     """입력받은 메시지를 파일로 저장한다.
-    1. 파일이 없으면 만든다.
+    1. 파일이 없으면 만든다. 만드는 위치는 홈디렉토리 아래이다.
     2. 파일이 있으면 파일 내용 맨 아래에 입력된 MSG 를 추가한다.
 
     Args:
-        msg:
+        msg: 추가할 내용, 문제
+        file_name: 저장파일 이름
 
     Returns:
 
     """
-    import os.path
-    home_dir = os.path.expanduser('~')
-    with open(f"{home_dir}/inf-question.txt", "a") as f:
+
+    home_dir = get_home_dir()
+    with open(f"{home_dir}/{file_name}", "a") as f:
         f.write(msg + "\n")
+
+
+def get_home_dir():
+    home_dir = os.path.expanduser('~')
+    return home_dir
--- tests/infinite_note/file/test_file_manager.py
+++ tests/infinite_note/file/test_file_manager.py
@@ -1,6 +1,21 @@
 from infinite_note.file import file_manager
+import os
 
 
 def test_save_question():
+    # given
     msg = "테스트 문제 추가"
-    file_manager.save_question(msg)
+    file_name = "inf-test.txt"
+    home_dir = file_manager.get_home_dir()
+
+    # when
+    file_manager.save_question(msg=msg, file_name=file_name)
+
+    # then
+    file_full_path = f"{home_dir}/{file_name}"
+    return_value = os.path.isfile(file_full_path)
+    assert return_value
+
+
+
+
